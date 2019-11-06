71e89445fafe76dd05a6e0480484c385acaea89e
1970 <1580426939@qq.com>
2019-11-06 17:43:00 +0800

    lambda表达式学习

--- src/main/java/demo/HelloController.java
+++ src/main/java/demo/HelloController.java
@@ -4,7 +4,17 @@ import Utils.Utils;
 import com.jfinal.core.Controller;
 import entity.Student;
 
+import java.sql.SQLOutput;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.TemporalAdjusters;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
+import java.util.function.Predicate;
 
 /**
  * @Description:  JFinal 框架学习
@@ -17,6 +27,34 @@ public class HelloController extends Controller {
         renderText("Hello, JFinal!");
     }
 
+    /**
+     * Lambda表达式：允许把函数作为一个方法的参数传递进方法中
+     */
+    public void lambdaTest() {
+        List<String> list = Arrays.asList("你", "是", "猪", "吗", "?", "猪悟能");
+//        list.forEach(System.out::println);
+
+
+        //条件过滤
+//        list.stream().filter((str) -> str.contains("猪")).forEach(System.out::println);
+
+        //条件组合过滤 (条件并列)
+        Predicate condition1 = (str) -> (((String)str).contains("猪"));
+        Predicate condition2 = (str) -> (((String)str).length() == 3);
+        list.stream().filter(condition1.and(condition2)).forEach(System.out::println);
+        renderText("Lambda表达式学习");
+    }
+
+    /**
+     * Stream学习
+     */
+    public void streamTest() {
+        renderText("Stream学习");
+    }
+
+    /**
+     * Optional类学习
+     */
     public void optionalTest() {
         Student student = new Student("戴敏", 25, "女");
         Optional<Student> stuOptional = Optional.ofNullable(student);
@@ -48,5 +86,44 @@ public class HelloController extends Controller {
         //orElseThrow 抛出特定异常
 //        optionalTest.orElseThrow(() -> new RuntimeException("你发生异常了你知道么！"));
         renderText("Optional类学习");
+    }
+
+    /**
+     * DateTime相关处理
+     */
+    public void dateTimeTest() {
+        LocalDate now = LocalDate.now();
+        int year = now.getYear();
+        int month = now.getMonthValue();
+        int day = now.getDayOfMonth();
+        System.out.println("now: " + now + " year:" + year + " month:" + month + " day:" + day);
+        System.out.println("isLeapYear:" + now.isLeapYear() + " dayOfYear:" + now.getDayOfYear() + " dayOfWeek:" + now.getDayOfWeek() + " lengthOfMonth:" + now.lengthOfMonth() + " lengthOfYear:" + now.lengthOfYear());
+
+        //自定义创建日期
+        LocalDate oneDay = LocalDate.of(2019, 11, 10);
+        LocalDate parseDay = LocalDate.parse("2019-12-24");
+        System.out.println("oneDay= " + oneDay + "   parseDay= " + parseDay);
+
+        //比较日期
+        System.out.println("比较日期 ：" + oneDay.equals(parseDay));
+
+        //加减日期
+        System.out.println("加减日期： " + now.plusDays(2) + "    " + now.minusDays(2));
+        //某天的开始时间和当天所在月的第一天
+        System.out.println("当天开始时间：" + now.atStartOfDay() + "  当天所在月的第一天： " + now.with(TemporalAdjusters.firstDayOfMonth()));
+
+        //localDateTime
+        LocalDateTime localDateTime = LocalDateTime.of(2019, 11, 06, 12, 30, 42);
+        LocalDateTime nowLocalDateTime = LocalDateTime.now();
+        System.out.println("LocalDate: " + now.atTime(LocalTime.MIN) + "   LocalDateTime: " + localDateTime  + " nowDateTime: " + nowLocalDateTime + " 拼接：" + LocalTime.now().atDate(LocalDate.now()));
+
+        //时间戳
+        System.out.println("Instant: " + Instant.now() + "    System: " + System.currentTimeMillis());
+        //格式化
+        String str = nowLocalDateTime.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME);
+        String str0 = nowLocalDateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-hh HH:mm:ss"));
+        System.out.println("str: " + str + "   str0: " + str0);
+
+        renderText("Java8 DATETIME新特性");
     }
 }
